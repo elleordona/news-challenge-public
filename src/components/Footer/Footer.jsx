0dc69c6b3d38c9264141cea764b6d4b263ce6892
Elle Ordona <eto0507@hotmail.co.uk>
2023-02-24 17:04:46 +0000

    test 2 passed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,1 +1,12 @@
 // Footer Component
+const Footer = () => {
+	return (
+		<footer>
+			<a href="http://github.com/digital-futures-academy" target="_blank" className="App-link" rel="noopener noreferrer">
+				Digital Futures Academy
+			</a>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,1 +1,15 @@
 // Testing for Footer Component
+
+// imports
+import { render } from '@testing-library/react';
+import Footer from './Footer';
+
+//* Test 2
+test('should render the same snapshot', () => {
+	// Arrange
+	const view = render(<Footer />);
+
+	// Act
+	// Assert
+	expect(view).toMatchSnapshot();
+});
