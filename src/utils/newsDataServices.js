fcaad96ea1d29c0bc10997a343895d09272999e1
Elle Ordona <eto0507@hotmail.co.uk>
2023-02-27 17:33:13 +0000

    added environment variables

--- src/utils/newsDataServices.js
+++ src/utils/newsDataServices.js
@@ -6,8 +6,11 @@ import axios from 'axios';
 
 export const getArticleDataAsync = async () => {
 	try {
-		const responseData = await axios.get('http://localhost:4000/results');
-		return responseData.data;
+		const responseData = await axios.get(process.env.REACT_APP_DATA_SERVICE_URL);
+
+		// if statement because test and development/production urls need different return values
+		if (process.env.REACT_APP_DATA_SERVICE_URL === 'http://localhost:4000/results') return responseData.data;
+		if (process.env.REACT_APP_DATA_SERVICE_URL === 'https://content.guardianapis.com/search?order-by=newest&show-fields=headline%2Cthumbnail%2Cbyline%2CbodyText&api-key=191cff44-153d-4f2f-ab2d-fa8eb74c39d7') return responseData.data.response.results;
 	} catch (e) {
 		return { error: `Error` };
 	}
