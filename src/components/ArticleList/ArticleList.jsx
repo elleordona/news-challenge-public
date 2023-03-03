49d8ad7f48ae63f3239223481e3b713c7e4a8c85
Elle Ordona <eto0507@hotmail.co.uk>
2023-03-03 15:25:58 +0000

    small code clean up

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -5,17 +5,21 @@ const ArticleList = (props) => {
 	const articleList = props.data.map((article) => (
 		<li key={article.id} className="card text-center w-75 mx-auto">
 			<img src={article.fields.thumbnail} alt="from guardian api" className="card-img-top rounded mx-auto" />
-			<br />
 			<p className="h5 card-title">{article.fields.headline}</p>
+			<p className="fs-6 card-subtitle text-muted">- {article.fields.byline}</p>
 		</li>
 	));
 
 	return (
 		<div>
-			<h2 className="display-5">{props.headingText}</h2>
+			<p className="display-5">{props.headingText}</p>
 			<ul title="articles">
 				{/* show loading info while data length is zero */}
-				{props.data.length === 0 && <li key="loading">Articles are loading...</li>}
+				{props.data.length === 0 && (
+					<li key="loading" className="h5">
+						Articles are loading...
+					</li>
+				)}
 				{/* show the list of articles is there are any*/}
 				{props.data.length > 0 && articleList}
 			</ul>
